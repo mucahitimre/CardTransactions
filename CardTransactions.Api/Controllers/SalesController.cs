f3e0945dc9c6e3efe80bed78d149bc151c4c7f27
Mucahit İmre <mucahit.imre@gmail.com>
2023-07-31 01:15:08 +0300

    CardTransactions api project created

---
+++ CardTransactions.Api/Controllers/SalesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CardTransactions.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SalesController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] SalesRequestModel request)
+        {
+
+        }
+    }
+
+    public class SalesRequestModel
+    {
+        public string CardNumber { get; set; }
+
+        public string CardFullName { get; set; }
+
+        public string CardEndDate { get; set; }
+
+        public int CardSecurityNumber { get; set; }
+
+        public double Price { get; set; }
+    }
+}
+
