80ac2402736671e892de73c22a6c29e05077fbae
Mücahit imre <mucahit.imre@gmail.com>
2023-08-01 13:04:34 +0300

    Web project created

---
+++ CardTransactions.Web/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CardTransactions.Api.Abstractions;
+using CardTransactions.Domain.Models;
+using CardTransactions.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CardTransactions.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISalesManager _salesManager;
+        private readonly ILogger<HomeController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="salesManager">The sales manager.</param>
+        /// <param name="logger">The logger.</param>
+        public HomeController(
+            ISalesManager salesManager,
+            ILogger<HomeController> logger)
+        {
+            _salesManager = salesManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _salesManager.GetListAsync(new SalesListFilter());
+
+            return View(list);
+        }
+
+        public async Task<IActionResult> GetData()
+        {
+            var list = await _salesManager.GetListAsync(new SalesListFilter());
+
+            return Json(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CardTransactions.Web/wwwroot/js/site.js
@@ -0,0 +1,89 @@
+﻿// Liste öğeleri için boş bir dizi tanımlıyoruz
+let itemList = [];
+
+// Liste sayfasındaki HTML elemanları seçme
+const itemListElement = document.getElementById('item-list');
+const idFilterInput = document.getElementById('id-filter');
+const dateFilterInput = document.getElementById('date-filter');
+const filterButton = document.getElementById('filter-button');
+
+// Detay sayfasındaki HTML elemanları seçme
+const detailContainer = document.getElementById('detail-container');
+
+// Verileri API'den al ve listeyi oluşturarak sayfada göster
+function fetchData() {
+    fetch('/Home/GetData') // API URL'ini buraya yazın
+        //.then(response => response.json())
+        .then(data => {
+            itemList = data;
+            renderList();
+        })
+        .catch(error => {
+            console.error('Veri alınamadı:', error);
+        });
+}
+
+// Listeyi oluştur ve sayfada göster
+function renderList() {
+    itemListElement.innerHTML = '';
+
+    itemList.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `ID: ${item.id} - Tarih: ${item.date}`;
+        listItem.setAttribute('data-detail', item.detail);
+
+        listItem.addEventListener('click', () => {
+            showDetail(item.id);
+        });
+
+        itemListElement.appendChild(listItem);
+    });
+}
+
+// Filtreleme işlemini gerçekleştir
+function filterList() {
+    const filteredItems = itemList.filter(item => {
+        const idFilter = idFilterInput.value.trim();
+        const dateFilter = dateFilterInput.value;
+
+        const idMatch = idFilter === '' || item.id.toString() === idFilter;
+        const dateMatch = dateFilter === '' || item.date === dateFilter;
+
+        return idMatch && dateMatch;
+    });
+
+    itemListElement.innerHTML = '';
+
+    filteredItems.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `ID: ${item.id} - Tarih: ${item.date}`;
+        listItem.setAttribute('data-detail', item.detail);
+
+        listItem.addEventListener('click', () => {
+            showDetail(item.id);
+        });
+
+        itemListElement.appendChild(listItem);
+    });
+}
+
+// Detayları göster
+function showDetail(itemId) {
+    const selectedItem = itemList.find(item => item.id === itemId);
+
+    if (selectedItem) {
+        detailContainer.innerHTML = `<p>ID: ${selectedItem.id}</p><p>Tarih: ${selectedItem.date}</p><p>Detay: ${selectedItem.detail}</p>`;
+    } else {
+        detailContainer.innerHTML = '<p>Detay bulunamadı.</p>';
+    }
+}
+
+// Sayfa yüklendiğinde veriyi al ve listeyi göster
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
+
+// Filtreleme butonuna tıklandığında listeyi filtrele
+filterButton.addEventListener('click', () => {
+    filterList();
+});
